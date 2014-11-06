270d3aec61d40374c36e13b1a769f7eb31bf125b
bev-a-tron <beverly.a.lau@gmail.com>
2014-11-05 21:48:19 -0800

    [BL] Filter by language

--- sorter.py
+++ sorter.py
@@ -4,13 +4,15 @@ class Movie:
 	year = ""
 	length = ""
 	genre = ""
+	language = ""
 
-	def __init__(self, id, title, year, length_in_minutes, genre):
+	def __init__(self, id = "", title = "", year = "", length_in_minutes = "", genre = "", language = ""):
 		self.id = id
 		self.title = title
 		self.year = year
 		self.length_in_minutes = length_in_minutes
 		self.genre = genre
+		self.language = language
 
 def file_parser(filename):
 	file = open(filename)
@@ -23,25 +25,34 @@ def file_parser(filename):
 		length = movie_info[5][0:-4].replace(',', '')
 		if length == "": length = 0
 		genre = movie_info[6]
+		language = movie_info[17]
 
-		this_movie = Movie(id, title, year, length, genre)
+		this_movie = Movie(id = id, title = title, year = year, length_in_minutes = length, genre = genre, language = language)
 		movies.append(this_movie)
 	return movies
 
 def print_movie_titles(movies):
 	for i, movie in enumerate(movies):
-		print movie.year, ": ", movie.title, "(%u)"%int(movie.length_in_minutes), ": ", movie.genre
+		print movie.year, ": ", movie.title, "(%u)"%int(movie.length_in_minutes), ": ", movie.genre, ": ", movie.language
 
-def filter_by_length_greater_than(movies, minutes):
+def filter_out_length_shorter_than(movies, minutes):
 	return [movie for movie in movies if int(movie.length_in_minutes) > 70]
 
-def filter_by_genre(movies, genre):
+def filter_out_genre(movies, genre):
 	return [movie for movie in movies if genre not in movie.genre]
+
+def filter_in_language(movies, language):
+	return [movie for movie in movies if movie.language in language]
 
 if __name__ == "__main__":
 	movies = file_parser("omdbextract.txt")
 	print_movie_titles(movies)
-	long_movies = filter_by_length_greater_than(movies, 70)
+	
+	long_movies = filter_out_length_shorter_than(movies, 70)
 	print_movie_titles(long_movies)
-	no_animation_movies = filter_by_genre(movies, "Animation")
+
+	no_animation_movies = filter_out_genre(movies, "Animation")
 	print_movie_titles(no_animation_movies)
+	
+	english_only_movies = filter_in_language(movies, "English")
+	print_movie_titles(english_only_movies)
--- test_sorter.py
+++ test_sorter.py
@@ -13,15 +13,21 @@ class TestSorter(unittest.TestCase):
 		self.assertEqual(self.movies[-2].title, "En la caja")
 
 	def test_should_only_show_movies_greater_than_70_mins(self):
-		long_movies = sorter.filter_by_length_greater_than(self.movies, 70)
+		long_movies = sorter.filter_out_length_shorter_than(self.movies, 70)
 		for movie in long_movies:
 			self.assertTrue(int(movie.length_in_minutes) > 70)
 
 	def test_should_not_show_animation(self):
-		genre = "Animation"
-		no_animation = sorter.filter_by_genre(self.movies, genre)
+		animation = "Animation"
+		no_animation = sorter.filter_out_genre(self.movies, animation)
 		for movie in no_animation:
-			self.assertTrue(genre not in movie.genre)
+			self.assertTrue(animation not in movie.genre)
+
+	def test_should_not_show_non_english(self):
+		english = "English"
+		only_english = sorter.filter_in_language(self.movies, english)
+		for movie in only_english:
+			self.assertTrue(english in movie.language)
 
 if __name__ == "__main__":
 	unittest.main()
