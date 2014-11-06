3cb61d0603e40cfe976bc71f33557eb6ec32c3ae
bev-a-tron <beverly.a.lau@gmail.com>
2014-11-05 19:23:53 -0800

    [BL] Filter by genre

--- sorter.py
+++ sorter.py
@@ -3,12 +3,14 @@ class Movie:
 	title = ""
 	year = ""
 	length = ""
+	genre = ""
 
-	def __init__(self, id, title, year, length_in_minutes):
+	def __init__(self, id, title, year, length_in_minutes, genre):
 		self.id = id
 		self.title = title
 		self.year = year
 		self.length_in_minutes = length_in_minutes
+		self.genre = genre
 
 def file_parser(filename):
 	file = open(filename)
@@ -20,20 +22,26 @@ def file_parser(filename):
 		year = movie_info[3][:4]
 		length = movie_info[5][0:-4].replace(',', '')
 		if length == "": length = 0
+		genre = movie_info[6]
 
-		this_movie = Movie(id, title, year, length)
+		this_movie = Movie(id, title, year, length, genre)
 		movies.append(this_movie)
 	return movies
 
 def print_movie_titles(movies):
 	for i, movie in enumerate(movies):
-		print movie.year, ": ", movie.title, "(%u)"%int(movie.length_in_minutes)
+		print movie.year, ": ", movie.title, "(%u)"%int(movie.length_in_minutes), ": ", movie.genre
 
-def length_over(movies, minutes):
+def filter_by_length_greater_than(movies, minutes):
 	return [movie for movie in movies if int(movie.length_in_minutes) > 70]
+
+def filter_by_genre(movies, genre):
+	return [movie for movie in movies if genre not in movie.genre]
 
 if __name__ == "__main__":
 	movies = file_parser("omdbextract.txt")
 	print_movie_titles(movies)
-	short_movies = length_over(movies, 70)
-	print_movie_titles(short_movies)
+	long_movies = filter_by_length_greater_than(movies, 70)
+	print_movie_titles(long_movies)
+	no_animation_movies = filter_by_genre(movies, "Animation")
+	print_movie_titles(no_animation_movies)
--- test_sorter.py
+++ test_sorter.py
@@ -3,18 +3,25 @@ import sorter
 
 class TestSorter(unittest.TestCase):
 
-	def test_should_parse_movie_titles(self):
-		movies = sorter.file_parser("omdbextract.txt")
-		self.assertEqual(movies[0].title, "Nobody Notices")
-		self.assertEqual(movies[1].title, "Tinder and Grinder")
-		self.assertEqual(movies[-1].title, "Autopsie d'un mariage blanc")
-		self.assertEqual(movies[-2].title, "En la caja")
+	def setUp(self):
+		self.movies = sorter.file_parser("omdbextract.txt")
 
-	def test_should_only_show_movies_less_than_70_mins(self):
-		movies = sorter.file_parser("omdbextract.txt")
-		short_movies = sorter.length_over(movies, 70)
-		for movie in short_movies:
+	def test_should_parse_movie_titles(self):
+		self.assertEqual(self.movies[0].title, "Nobody Notices")
+		self.assertEqual(self.movies[1].title, "Tinder and Grinder")
+		self.assertEqual(self.movies[-1].title, "Autopsie d'un mariage blanc")
+		self.assertEqual(self.movies[-2].title, "En la caja")
+
+	def test_should_only_show_movies_greater_than_70_mins(self):
+		long_movies = sorter.filter_by_length_greater_than(self.movies, 70)
+		for movie in long_movies:
 			self.assertTrue(int(movie.length_in_minutes) > 70)
+
+	def test_should_not_show_animation(self):
+		genre = "Animation"
+		no_animation = sorter.filter_by_genre(self.movies, genre)
+		for movie in no_animation:
+			self.assertTrue(genre not in movie.genre)
 
 if __name__ == "__main__":
 	unittest.main()
